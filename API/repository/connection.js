0c42f027763cd04d9fff09b42ce3757becdd1d32
Macys1704 <cherin1704@gmail.com>
2023-09-24 09:40:58 -0300

    .

---
+++ API/repository/connection.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise'
+
+const conexao = await mysql.createConnection({
+    host: process.env.HOST,
+    database: process.env.DB,
+    user: process.env.USER,
+    password: process.env.PWD
+});
+
+
+console.log('BD connect')
+export default conexao;
